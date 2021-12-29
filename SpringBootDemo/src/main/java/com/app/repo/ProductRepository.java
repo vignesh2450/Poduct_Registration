17a829902c5fd6610011df32a62b296895d2a501
balasubrahmanyam <balasubrahmanyam321@gmail.com>
2021-12-29 09:53:49 +0530

    deleted spring boot folder

--- SpringBootDemo/src/main/java/com/app/repo/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.app.model.Product;
-@Repository
-public interface ProductRepository extends JpaRepository <Product, Integer> {
-Product getProductById(Integer id);
-}
